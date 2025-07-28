ef28be433dee553f2ef2742dd52691b4ae008542
amit <amitsarker95@gmail.com>
2025-07-28 13:01:11 +0600

    TopicAdmin and RevisionLog Admin upgraded

--- tracker/admin.py
+++ tracker/admin.py
@@ -45,6 +45,30 @@ class SubjecsAdmin(admin.ModelAdmin):
         super().save_model(request, object, form, change)
 
 
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'title', 'description', 'created_at', 'is_completed',
+                    'total_revisions', 'last_revised', 'deadline', 'is_completed')
+    list_filter = ('subject__user', 'is_completed', 'deadline')
+    list_display_links = ('title', )
+    search_fields = ('title', 'subject__name', 'subject__user__full_name', 'subject__user__email')
+    readonly_fields = ('created_at', 'total_revisions', 'last_revised')
 
-admin.site.register(Topic)
-admin.site.register(RevisionLog)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(subject__user=request.user)
+    
+    def save_model(self, request, object, form, change):
+        if not object.subject.user:
+            object.subject.user = request.user
+        super().save_model(request, object, form, change)
+
+@admin.register(RevisionLog)
+class RevisionLogAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'revised_at', 'notes')
+    list_filter = ('topic__subject__user', 'revised_at')
+    list_display_links = ('topic', )
+    search_fields = ('topic__title', 'topic__subject__name', 'topic__subject__user__full_name', 'topic__subject__user__email')
+    readonly_fields = ('revised_at',)
